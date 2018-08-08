8cd34d1a0babb36d32f5a48f6630eb96fa93bc7e
Spencer Greene <spencerwgreene@gmail.com>
2018-08-08 10:20:29 -0700

    updates bugs found in Lighthouse report

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@
 /* ----------------------------------------------- */
 
 /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-particlesJS.load('particles-js', 'particles.json', function() {
-  
-});
+// particlesJS.load('particles-js', 'particles.json', function() {
+//
+// });
 
 /* Otherwise just put the config content (json): */
 //particlesJS("particles-js", json);
