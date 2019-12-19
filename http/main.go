d5f70d85268f47f1a035bb8a3215e5db5f676665
Spencer Greene <spencerwgreene@gmail.com>
2019-12-18 16:17:33 -0800

    Add logging to a file instead of stdout.

--- http/main.go
+++ http/main.go
@@ -3,11 +3,18 @@ package main
 
 import (
 	"log"
+	"os"
+	"io"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	f, err := os.Create("server.log")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	gin.DefaultWriter = io.MultiWriter(f)
 	r := gin.Default()
 	r.Static("/", "./site")
 	if err := r.Run(); err != nil {
