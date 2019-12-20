4352e1e4eab47342f598ddf035a4f193e0b169bb
Spencer Greene <spencerwgreene@gmail.com>
2019-12-20 00:24:01 -0800

    Change parse_log.py to print common log format strings.

--- parse_log.py
+++ parse_log.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     lines = [[x.strip() for x in line.split('|')] for line in lines if len(line) > 5 and line[:5] == "[GIN]"]
     for line in lines:
         time = datetime.datetime.strptime(line[0], "[GIN] %Y/%m/%d - %H:%M:%S")
+        time = time.astimezone(datetime.timezone(datetime.timedelta(0)))
         status = int(line[1])
         if line[2][-2:] == "µs":
             latency_ns = float(line[2][:-2])
@@ -23,7 +24,9 @@ if __name__ == "__main__":
         spl = line[4].split()
         method = spl[0]
         route = spl[1]
-        print(json.dumps({
-            "time": str(time), "status": status, "latency_ns": latency_ns, "ip": ip, "method": method, "route": route,
-        }))
+        # print(json.dumps({
+        #     "time": str(time), "status": status, "latency_ns": latency_ns, "ip": ip, "method": method, "route": route,
+        # }))
+        fmt_str = "%d/%b/%Y:%H:%M:%S %z"
+        print(ip, "-", "-", f"[{time.strftime(fmt_str)}]", f"\"{method} {route}\"", status, "-")
 
