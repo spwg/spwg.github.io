b6482d25891871b9fca89df145d4ad8c3ae121f1
Spencer Greene <spencerwgreene@gmail.com>
2023-07-09 14:52:35 -0700

    remove special case logging, 404 handling, and tls because this will run on fly.io

--- http/main.go
+++ http/main.go
@@ -2,53 +2,17 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"log"
-	"net/http"
 	"os"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/unrolled/secure"
 )
 
-func setupLogs() error {
-	if _, err := os.Stat("./logs"); os.IsNotExist(err) {
-		if err := os.Mkdir("./logs", 0777); err != nil {
-			return err
-		}
-	}
-	now := time.Now().UTC()
-	errorLog, err := os.Create(fmt.Sprintf("./logs/error %s.log", now.Format(time.RFC822)))
-	if err != nil {
-		return err
-	}
-	serverLog, err := os.Create(fmt.Sprintf("./logs/gin %s.log", now.Format(time.RFC822)))
-	if err != nil {
-		return err
-	}
-	gin.DefaultWriter = io.MultiWriter(serverLog)
-	gin.DefaultErrorWriter = io.MultiWriter(errorLog)
-	return nil
-}
-
 func setupMiddleware(r *gin.Engine) {
 	// Setup logging and recovery first so that the logging happens first
 	// and then recovery happens before any other middleware.
-	r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
-		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s\" \"%s\" \"%s\"\n",
-			param.ClientIP,
-			param.TimeStamp.Format(time.RFC1123),
-			param.Method,
-			param.Path,
-			param.Request.Proto,
-			param.StatusCode,
-			param.Latency,
-			param.Request.UserAgent(),
-			param.ErrorMessage,
-		)
-	}))
+	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 	secureMiddleware := secure.New(secure.Options{
 		AllowedHosts:  []string{"spencergreene.com", "www.spencergreene.com"},
@@ -70,28 +34,15 @@ func setupMiddleware(r *gin.Engine) {
 }
 
 func main() {
-	if err := setupLogs(); err != nil {
-		log.Fatalln(err)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
 	}
 	r := gin.New()
 	setupMiddleware(r)
 	r.Static("/", "./site")
-	r.LoadHTMLFiles("./templates/404.tmpl")
-	r.NoRoute(func(c *gin.Context) {
-		c.HTML(http.StatusNotFound, "404.tmpl", gin.H{
-			"path": c.Param("filepath"),
-		})
-	})
-	if gin.IsDebugging() {
-		if err := r.Run(":8080"); err != nil {
-			log.Fatalln(err)
-		}
-		return
-	}
-	if err := r.RunTLS(
-		":8080",
-		"/etc/letsencrypt/live/spencergreene.com/fullchain.pem",
-		"/etc/letsencrypt/live/spencergreene.com/privkey.pem"); err != nil {
+	gin.SetMode(gin.ReleaseMode)
+	if err := r.Run(":" + port); err != nil {
 		log.Fatalln(err)
 	}
 }
