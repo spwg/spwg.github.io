a3151ebb79bbf1c4a326d87b1776247a1bfb7d46
Spencer Greene <spencerwgreene@gmail.com>
2023-12-17 12:03:35 -0500

    Refactor route registration.

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -16,6 +16,11 @@ import (
 	"golang.org/x/exp/slices"
 )
 
+type server struct {
+	static fs.FS
+	t      *template.Template
+}
+
 type dnsPage struct {
 	Host        string
 	IPAddresses []string
@@ -23,103 +28,98 @@ type dnsPage struct {
 	NextReload  string
 }
 
-// dnsChecker is the endpoint for the dns tool.
-func dnsChecker(t *template.Template) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		ctx := c.Request.Context()
-		h, ok := c.GetQuery("host")
-		if !ok {
-			if err := t.Execute(c.Writer, map[string]any{"Result": dnsPage{}}); err != nil {
-				c.Error(err)
-			}
-			return
+func (s *server) dnsChecker(c *gin.Context) {
+	ctx := c.Request.Context()
+	h, ok := c.GetQuery("host")
+	if !ok {
+		if err := s.t.Execute(c.Writer, map[string]any{"Result": dnsPage{}}); err != nil {
+			c.Error(err)
 		}
-		lookupHostResponse, err := net.DefaultResolver.LookupHost(ctx, h)
-		if err != nil {
-			log.Print(err)
-			switch err.(type) {
-			case *net.DNSError:
-			default:
-				c.AbortWithError(500, err)
-				return
-			}
-		}
-		slices.Sort(lookupHostResponse)
-		lookupNSResponse, err := net.DefaultResolver.LookupNS(ctx, h)
-		if err != nil {
-			log.Print(err)
-			switch err.(type) {
-			case *net.DNSError:
-			default:
-				c.AbortWithError(500, err)
-				return
-			}
-		}
-		slices.SortFunc(lookupNSResponse, func(a, b *net.NS) bool {
-			return a.Host < b.Host
-		})
-		var nameServers []string
-		for _, n := range lookupNSResponse {
-			nameServers = append(nameServers, n.Host)
-		}
-		now := time.Now()
-		r := dnsPage{
-			Host:        h,
-			IPAddresses: lookupHostResponse,
-			NameServers: nameServers,
-			NextReload:  now.UTC().Add(time.Minute).Format(time.RFC3339),
-		}
-		// This page uses htmx, which lets the server return html content to update just part of the page.
-		var b bytes.Buffer
-		if c.GetHeader("HX-Request") != "" {
-			// Just execute return the HTML needed to update the page's result element.
-			err = t.ExecuteTemplate(&b, "dns_result", r)
-		} else {
-			// Go nested templates can only receive 1 argument.
-			err = t.Execute(&b, map[string]any{"Result": r})
-		}
-		if err != nil {
+		return
+	}
+	lookupHostResponse, err := net.DefaultResolver.LookupHost(ctx, h)
+	if err != nil {
+		log.Print(err)
+		switch err.(type) {
+		case *net.DNSError:
+		default:
 			c.AbortWithError(500, err)
 			return
 		}
-		if _, err := c.Writer.Write(b.Bytes()); err != nil {
-			log.Print(err)
+	}
+	slices.Sort(lookupHostResponse)
+	lookupNSResponse, err := net.DefaultResolver.LookupNS(ctx, h)
+	if err != nil {
+		log.Print(err)
+		switch err.(type) {
+		case *net.DNSError:
+		default:
+			c.AbortWithError(500, err)
+			return
 		}
+	}
+	slices.SortFunc(lookupNSResponse, func(a, b *net.NS) bool {
+		return a.Host < b.Host
+	})
+	var nameServers []string
+	for _, n := range lookupNSResponse {
+		nameServers = append(nameServers, n.Host)
+	}
+	now := time.Now()
+	r := dnsPage{
+		Host:        h,
+		IPAddresses: lookupHostResponse,
+		NameServers: nameServers,
+		NextReload:  now.UTC().Add(time.Minute).Format(time.RFC3339),
+	}
+	// This page uses htmx, which lets the server return html content to update just part of the page.
+	var b bytes.Buffer
+	if c.GetHeader("HX-Request") != "" {
+		// Just execute return the HTML needed to update the page's result element.
+		err = s.t.ExecuteTemplate(&b, "dns_result", r)
+	} else {
+		// Go nested templates can only receive 1 argument.
+		err = s.t.Execute(&b, map[string]any{"Result": r})
+	}
+	if err != nil {
+		c.AbortWithError(500, err)
+		return
+	}
+	if _, err := c.Writer.Write(b.Bytes()); err != nil {
+		log.Print(err)
 	}
 }
 
-// AircraftFeed is the endpoint for aircraft data feed.
-func AircraftFeed(c *gin.Context) {
-	// TODO: configure the service to read from gcs, rate limiting, and making
-	// the web page.
+// aircraftFeed is the endpoint for aircraft data feed.
+func (s *server) aircraftFeed(c *gin.Context) {
+	// TODO:gcs service configuration, rate limiting, the web page, and an integration test.
+	//
+	// For now, just display the stats.json file.
+}
+
+func (s *server) root(c *gin.Context) {
+	c.FileFromFS(c.Request.URL.Path, http.FS(s.static))
+}
+
+func (s *server) js(c *gin.Context) {
+	c.FileFromFS(path.Base(c.Request.URL.Path), http.FS(s.static))
+}
+
+func (s *server) css(c *gin.Context) {
+	c.FileFromFS(c.Params.ByName("path"), http.FS(s.static))
 }
 
 // InstallRoutes registers the server's routes on the given [*gin.Engine].
-func InstallRoutes(sys fs.FS, engine *gin.Engine) *gin.Engine {
-	installStaticRoutes(sys, engine)
-	installDNSRoutes(sys, engine)
-	return engine
-}
-
-// installStaticRoutes registers the routes for static pages on the engine.
-func installStaticRoutes(staticFS fs.FS, engine *gin.Engine) {
-	engine.GET("/", func(c *gin.Context) {
-		c.FileFromFS(c.Request.URL.Path, http.FS(staticFS))
-	})
-	engine.GET("/js/:path", func(c *gin.Context) {
-		c.FileFromFS(path.Base(c.Request.URL.Path), http.FS(staticFS))
-	})
-	engine.GET("/css/:path", func(c *gin.Context) {
-		c.FileFromFS(c.Params.ByName("path"), http.FS(staticFS))
-	})
-}
-
-// installDNSRoutes registers the routes for the dns checker pages on the
-// engine.
-func installDNSRoutes(staticFS fs.FS, engine *gin.Engine) {
-	t, err := template.ParseFS(staticFS, "dnschecker.tmpl", "dnsresult.tmpl")
+func InstallRoutes(static fs.FS, engine *gin.Engine) *gin.Engine {
+	t, err := template.ParseFS(static, "dnschecker.tmpl", "dnsresult.tmpl")
 	if err != nil {
 		log.Fatal(err)
 	}
-	engine.GET("/dnschecker", dnsChecker(t))
+	s := &server{static, t}
+	engine.GET("/", s.root)
+	engine.GET("/js/:path", s.js)
+	engine.GET("/css/:path", s.css)
+	engine.GET("/dnschecker", s.dnsChecker)
+	engine.GET("/aircraft/feed", s.aircraftFeed)
+	return engine
 }
