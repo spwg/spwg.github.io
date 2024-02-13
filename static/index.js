7dbf0e32d62068089057d734f7985e26394cad24
Spencer Greene <spencerwgreene@gmail.com>
2024-02-13 00:11:25 -0500

    delete commented out code

--- static/index.js
+++ static/index.js
@@ -1,16 +1,4 @@
 window.onload = function () {
-    // const observer = new PerformanceObserver((list) => {
-    //     list.getEntries().forEach((entry) => {
-    //         const domContentLoadedTime =
-    //             entry.domContentLoadedEventEnd - entry.domContentLoadedEventStart;
-    //         console.log(
-    //             `${entry.name}: DOMContentLoaded processing time: ${domContentLoadedTime}ms`,
-    //         );
-    //     });
-    // });
-
-    // observer.observe({ type: "navigation", buffered: true });
-
     const entries = performance.getEntriesByType("navigation");
     if (entries.length !== 1) {
         console.warn("Expected 1 navigation event, got", entries);
